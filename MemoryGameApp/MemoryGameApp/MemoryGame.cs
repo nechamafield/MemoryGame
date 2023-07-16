06de3462c89c57b7e70b5ac5c2a5e6ca2c928a9c
nechamafield <nechamafield@gmail.com>
2023-07-16 12:59:33 -0400

    ClickedButtons Works - works for 1st turn. need to fix NextTurn

--- MemoryGameApp/MemoryGameApp/MemoryGame.cs
+++ MemoryGameApp/MemoryGameApp/MemoryGame.cs
@@ -22,7 +22,7 @@ namespace MemoryGameApp
 
         enum MatchedEnum { Matched, NotMatched };
         MatchedEnum IfMatched = MatchedEnum.NotMatched;
-        
+
         // dictionary<String, Color> colorMatches
         // private void SetColors{
         // foreach btn in lstbuttons
@@ -63,11 +63,16 @@ namespace MemoryGameApp
                 new(){btn61, btn51},
                 new(){btn45, btn62},
             };
-            
+            foreach (List<Button> sublist in lstmatchingsets)
+            {
+                var c = GetRandomBackColor();
+                sublist.ForEach(b => b.Tag = c);
+            }
         }
 
-        private void NextTurn()
+        private void NextTurn(Button btn)
         {
+            DoTurn(btn);
             EnableButtons(true);
             int n = 0;
             bool bn = int.TryParse(lblTurnNumber.Text, out n);
@@ -79,8 +84,8 @@ namespace MemoryGameApp
         {
             if (sender is Button)
             {
-                NextTurn();
-                ReSetBtns();
+                NextTurn((Button)sender);
+                //ReSetBtns();
                 //AddOneScore();
             }
         }
@@ -101,21 +106,10 @@ namespace MemoryGameApp
         //makes score quadruple
         private void AddOneScore()
         {
-            var lstmatchedcolors = new List<Color> { };
-            Color c = Color.Black;
-            if (btn1test.BackColor == btn2test.BackColor)
-            {
-                c = btn1test.BackColor;
-            }
-
-            if (btn1test.BackColor == btn2test.BackColor && lstmatchedcolors.Contains(c) == false)
-            {
-                int n = 0;
-                bool bn = int.TryParse(lblScoreNum.Text, out n);
-                int scorenum = n + 1;
-                lblScoreNum.Text = scorenum.ToString();
-            }
-            lstmatchedcolors.Add(c);
+            int n = 0;
+            bool bn = int.TryParse(lblScoreNum.Text, out n);
+            int scorenum = n + 1;
+            lblScoreNum.Text = scorenum.ToString();
         }
 
         private Color GetRandomBackColor(int minr, int maxr, int ming, int maxg, int minb, int maxb)
@@ -144,90 +138,66 @@ namespace MemoryGameApp
 
         }
 
-        public void ReSetBtns()
-        {
-            btn1test = new();
-            btn2test = new();
-        }
-
-        //btn, 1st button clicked, 2nd button clicked, should be same color if they are a set
+        //public void ReSetBtns()
+        //{
+        //    btn1test = new();
+        //    btn2test = new();
+        //}
 
         private void DoTurn(Button btn)
         {
             SetButtons(btn);
-            //Color c = GetRandomBackColor();
-            ClickedButtons(btn, btn1test, btn2test); //, c);
-            ReSetBtns();
 
-            //ClickedButtons(btn, btn33, btn56, lstcolor[0]);
-            //ClickedButtons(btn, btn53, btn11, lstcolor[2]);
-            //ClickedButtons(btn, btn12, btn46, lstcolor[3]);
-            //ClickedButtons(btn, btn13, btn66, lstcolor[4]);
-            //ClickedButtons(btn, btn14, btn42, lstcolor[5]);
-            //ClickedButtons(btn, btn15, btn64, lstcolor[6]);
-            //ClickedButtons(btn, btn16, btn21, lstcolor[7]);
-            //ClickedButtons(btn, btn22, btn55, lstcolor[8]);
-            //ClickedButtons(btn, btn23, btn52, lstcolor[9]);
-            //ClickedButtons(btn, btn24, btn41, lstcolor[10]);
-            //ClickedButtons(btn, btn25, btn26, lstcolor[11]);
-            //ClickedButtons(btn, btn31, btn36, lstcolor[12]);
-            //ClickedButtons(btn, btn32, btn43, lstcolor[13]); 
-            //ClickedButtons(btn, btn34, btn65, lstcolor[14]);
-            //ClickedButtons(btn, btn35, btn63, lstcolor[15]);
-            //ClickedButtons(btn, btn44, btn54, lstcolor[16]);
-            //ClickedButtons(btn, btn61, btn51, lstcolor[17]);
-            //ClickedButtons(btn, btn45, btn62, lstcolor[1]);
-            //if (lstbuttons.Count(b => b.BackColor != Color.LightSteelBlue) >= 2)// && lstmatchingsets.Contains(btn1.BackColor))
-            //{
-            //    EnableButtons(false);
-            //}
+            ClickedButtons(btn);  //, btn1test, btn2test);
+            //ReSetBtns(); - causes it to keep putting clicked btn as btn1test cuz its resetting it to ""
+
+            if (lstbuttons.Count(b => b.BackColor != Color.LightSteelBlue) >= 2)// && lstmatchingsets.Contains(btn1.BackColor))
+            {
+                EnableButtons(false);
+            }
         }
 
 
-        private void ClickedButtons(Button btn, Button btn1, Button btn2)//, Color c)
+        private void ClickedButtons(Button btn)  //, Button btn1, Button btn2)
         {
             ////////////////////////////////
-            //YF work
-
             //do all matching work btwn set and reset buttons
-            
-            foreach (List<Button> sublist in lstmatchingsets)
+
+            if (btn == btn1test || btn == btn2test)
             {
-                var c = GetRandomBackColor();
-                ///////im trying to make the buttons in each sublist be the same random color
-                sublist.ForEach(b => b.BackColor = c);
-
-                if (sublist.Contains(btn1test) == true && sublist.Contains(btn2test) == true)
-                //if (btn1test.BackColor == btn2test.BackColor)
-                {
-                    AddOneScore();
-                    //should change color either way
-                    //sublist.ForEach(b => b.BackColor = c);
-                }
-                else 
-                {
-
-                }
+                btn.BackColor = (Color)btn.Tag;
             }
-            //if btn1test and btn2test is a set in matchedsets then color should be the same random color from lstcolor
-            //if (btn == btn1test || btn == btn2test)
-            //{
-            //    btn.BackColor = c;
-            //}
 
-            //AddOneScore();
-
-            
-
-            ////////////////////////////
-            //if (btn == btn1 || btn == btn2)
-            //{
-            //    btn.BackColor = c;
-            //}
-
-
-
+            if (btn1test.BackColor == btn2test.BackColor)
+            {
+               AddOneScore();
+            }
+            else
+            {
+                lblScoreNum.Text = lblScoreNum.Text;
+            }
         }
+
+
+        //if btn1test and btn2test is a set in matchedsets then color should be the same random color from lstcolor
+        //if (btn == btn1test || btn == btn2test)
+        //{
+        //    btn.BackColor = c;
+        //}
+
+        //AddOneScore();
+
+
+
+        ////////////////////////////
+        //if (btn == btn1 || btn == btn2)
+        //{
+        //    btn.BackColor = c;
+        //}
+
+
+
+        //}
 
         private void B_Click(object? sender, EventArgs e)
         {
